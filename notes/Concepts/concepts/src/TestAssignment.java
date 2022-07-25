55a64dba92d3759c50d5688ba280ff9dd8bf7e35
Satha Sivam <satha.sivam@outlook.com>
2022-07-25 18:38:11 +0800

    25 July 2022

--- notes/Concepts/concepts/src/TestAssignment.java
+++ notes/Concepts/concepts/src/TestAssignment.java
@@ -1,5 +1,13 @@
-
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class TestAssignment {
-}
+
+    public static void main(String[] args) {
+        int start = 13;
+        int stop = 21;
+
+        for(int i = start; i < stop; i++){
+            System.out.println("beep!");
+        }
+    } }
